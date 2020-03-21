2e3535833b268e7b170ec3ae8d3617676cf64b2d
Carlos Gándara <xoubaman@gmail.com>
2020-03-21 11:30:29 +0100

    Update install script to be used via wget

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-read -p "Install all the junk? [y/n] " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  echo ">>>>>>>>> ಠ_ಠ -(Let's do this!)"
-      source scripts/install-apps.sh
-  done
-fi
-
-source scripts/update-system-dotfiles.sh
+echo ">>>>>>>>> ಠ_ಠ -(Let's do this!)"
+echo "Installing git"
+sudo apt update
+sudo apt install -y git
+echo "Clonning the repository in /projects/my-config"
+git clone https://github.com/xoubaman/my-config.git ${HOME}/projects/my-config
+source ${HOME}/projects/my-config/scripts/install-apps.sh
+source ${HOME}/projects/my-config/scripts/update-system-dotfiles.sh
 
 echo ">>>>>>>>> (-■_■) Good to go!"
