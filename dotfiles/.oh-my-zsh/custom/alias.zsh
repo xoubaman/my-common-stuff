4eb2094ec073d110294bd125317190df19481236
Carlos Gándara <xoubaman@gmail.com>
2020-05-16 13:17:49 +0200

    Aliases for dockerized PHP

--- dotfiles/.oh-my-zsh/custom/alias.zsh
+++ dotfiles/.oh-my-zsh/custom/alias.zsh
@@ -4,3 +4,8 @@ alias g="git"
 #Docker
 alias doco="docker-compose"
 alias docoup="docker-compose up -d --force-recreate"
+
+#PHP
+alias php74="docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) xoubaman/php:7.4-dev"
+alias php74-composer="php74 composer"
+alias php74-phpunit="php74 vendor/bin/phpunit"
