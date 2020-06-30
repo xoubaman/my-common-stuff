bd94b2ba1c61e41c4883aa56b5660d457579f23c
Carlos Gándara <xoubaman@gmail.com>
2020-06-30 21:04:38 +0200

    PHP, ls, cat alias

--- dotfiles/.oh-my-zsh/custom/alias.zsh
+++ dotfiles/.oh-my-zsh/custom/alias.zsh
@@ -9,3 +9,7 @@ alias docoup="docker-compose up -d --force-recreate"
 alias php74="docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) xoubaman/php:7.4-dev"
 alias php74-composer="php74 composer"
 alias php74-phpunit="php74 vendor/bin/phpunit"
+
+#Misc
+alias ls="lsd"
+alias cat="bat"
