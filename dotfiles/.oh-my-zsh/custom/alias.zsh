c0941649a096b7866b9e8e6669bac600ffd883b7
Carlos Gándara <xoubaman@gmail.com>
2020-12-03 20:09:56 +0100

    Single quotes on bash alias ¬¬, use bat for branch list in git

--- dotfiles/.oh-my-zsh/custom/alias.zsh
+++ dotfiles/.oh-my-zsh/custom/alias.zsh
@@ -1,15 +1,16 @@
 #Git
-alias g="git"
+alias g='git'
 
 #Docker
-alias doco="docker-compose"
-alias docoup="docker-compose up -d --force-recreate"
+alias doco='docker-compose'
+alias docoup='docker-compose up -d --force-recreate'
 
 #PHP
-alias php74="docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) xoubaman/php:7.4-dev"
-alias php74-composer="php74 composer"
-alias php74-phpunit="php74 vendor/bin/phpunit"
+alias php72='docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) xoubaman/php:7.2-dev'
+alias php73='docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) xoubaman/php:7.3-dev'
+alias php74='docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) xoubaman/php:7.4-dev'
+alias laravel='docker run --rm -v $(pwd):/app -u $(id -u):$(id -g) -p 127.0.0.1:8000:5000 xoubaman/php:7.4-dev'
 
 #Misc
-alias ls="lsd"
-alias cat="bat"
+alias ls='lsd'
+alias cat='bat'
